736275e4787f214cb4e2c628b2083124b1acb644
SURYA <suryasanthoshh@gmail.com>
2026-07-08 16:03:20 +0530

    login v2

--- src/Pages/Publics/Home.jsx
+++ src/Pages/Publics/Home.jsx
@@ -4,7 +4,7 @@ export default function Home() {
     return (
         <div className="h-screen flex justify-center items-center ">
             <NavLink to="/admin/login" >
-                <button className=' hover:cursor-pointer hover:-translate-y-[2px] hover:scale-101 active:scale-99 transition  px-5 py-2 bg-blue-300 font-bold  rounded m-1'>Admin Login</button>
+                <button className=' hover:cursor-pointer hover:-translate-y-[2px] hover:scale-101 active:scale-99 transition  px-5 py-2 bg-blue-300 font-bold  rounded m-1'>Staff Login</button>
             </NavLink>
             <NavLink to='/student/login'>
                 <button className=' hover:cursor-pointer hover:-translate-y-[2px] hover:scale-101 active:scale-99 transition  px-5 py-2 bg-green-300 font-bold  rounded m-1'>Student Login</button>
